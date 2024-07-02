65b5668fafe2d69e1b213d3066d90011623f6e81
ashah001 <ataul.shahid@student.hs-rm.de>
2024-07-02 14:14:08 +0200

    Change cololr of Header and Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,6 @@
-
 export default function Footer() {
     return (
-        <footer className="bg-gray-200 py-4 text-center">
+        <footer className="bg-gradient-to-r from-gray-800 via-gray-700 to-gray-600 py-4 text-center text-white">
             <p className="text-1xl font-bold">Powered by OpenWeatherApi</p>
         </footer>
     );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,19 @@
 import { TiWeatherPartlySunny } from "react-icons/ti";
 import Link from "next/link";
 
-
 type Props = {};
 
-export default function Navbar({ }: Props) {
+export default function Navbar({}: Props) {
   return (
-    <header className="sticky top-0 z-50 bg-gray-200">
+    <nav className="sticky top-0 z-50 bg-gradient-to-r from-gray-800 via-gray-700 to-gray-600 text-white">
       <div className="h-[100px] flex justify-center items-center px-3">
         <div className="flex items-center justify-center gap-2">
           <div className="flex items-center justify-center">
             <TiWeatherPartlySunny className="text-3xl font-bold" />
           </div>
-          <Link href="http://localhost:3000" className="text-3xl font-bold">HSRM-Wetter-App</Link>
+          <Link href="http://localhost:3000" className="text-3xl font-bold text-white">HSRM-Wetter-App</Link>
         </div>
       </div>
-    </header>
+    </nav>
   );
 }
