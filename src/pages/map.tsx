f927578ff0994734a4b609f8395c1329e76a68d7
ashah001 <ataul.shahid@student.hs-rm.de>
2024-07-03 19:07:09 +0200

    add "zoom out to see more detail" prompt

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -19,11 +19,15 @@ export default function MapPage() {
     <div className="min-h-screen flex flex-col justify-between bg-gray-100">
       <Header />
       <main className="flex-grow flex flex-col items-center mt-5 px-4">
+      <div className="mb-4 w-full max-w-3xl p-3 bg-gray-700 text-white text-center rounded-lg shadow-lg">
+          <p className="text-base font-semibold">Map of {city}</p>
+        </div>
+     
         <div className="w-full max-w-3xl h-[50vh] md:h-[60vh] lg:h-[70vh] rounded-lg shadow-lg overflow-hidden">
           <Map lat={parseFloat(lat as string)} lon={parseFloat(lon as string)} />
         </div>
-        <div className="mt-4 text-center">
-          <p className="text-lg font-semibold">Map of {city}</p>
+        <div className="mb-4 mt-1 w-full max-w-3xl p-3 bg-gray-700 text-white text-center rounded-lg shadow-lg">
+          <p className="text-base font-semibold">Zoom out to see more details</p>
         </div>
       </main>
       <Footer />
