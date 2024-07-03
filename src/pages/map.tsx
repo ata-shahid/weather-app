be6248270902a0c65940d98178992cc6d0c7009d
ashah001 <ataul.shahid@student.hs-rm.de>
2024-07-03 19:53:40 +0200

    add "Back to Forecast" button

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -14,11 +14,23 @@ export default function MapPage() {
   if (!lat || !lon || !city) {
     return <div>Error: Latitude, Longitude, and City are required to display the map.</div>;
   }
+  // to go back to the forecast page without fetching the data again
+  const handleBack = () => {
+    router.back();
+  };
 
   return (
     <div className="min-h-screen flex flex-col justify-between bg-gray-100">
       <Header />
       <main className="flex-grow flex flex-col items-center mt-5 px-4">
+      <div className="w-full flex justify-start mb-4">
+          <button
+            onClick={handleBack}
+            className="text-white bg-gradient-to-r from-gray-800 via-gray-700 to-gray-600 hover:bg-gradient-to-l font-semibold text-lg p-3 border border-gray-700 rounded-lg shadow-md transition duration-300 ease-in-out transform hover:-translate-y-1 hover:shadow-lg"
+          >
+            Back to Forecast
+          </button>
+        </div>
       <div className="mb-4 w-full max-w-3xl p-3 bg-gray-700 text-white text-center rounded-lg shadow-lg">
           <p className="text-base font-semibold">Map of {city}</p>
         </div>
@@ -29,6 +41,8 @@ export default function MapPage() {
         <div className="mb-4 mt-1 w-full max-w-3xl p-3 bg-gray-700 text-white text-center rounded-lg shadow-lg">
           <p className="text-base font-semibold">Zoom out to see more details</p>
         </div>
+       
+      
       </main>
       <Footer />
     </div>
